ec4548de41167f48d2062b79a69b08b9b7149112
chimikoo <chimikoo@gmail.com>
2023-05-08 14:21:43 +0200

    Cleaning up some code re-styling button

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,9 @@
-/* This function slows down the video by 15% 1 being 100% */
+
 const video = document.getElementById("background-video");
 const container = document.getElementById("scroll-listener");
 const button = document.getElementById("back-to-top-button");
 let scrollTop;
+
 
 const scrollFunction = () => {
   if (scrollTop > 100) {
@@ -11,6 +12,7 @@ const scrollFunction = () => {
     button.style.display = "none";
   }
 };
+
 
 container.addEventListener("scroll", () => {
   scrollTop = container.scrollTop;
@@ -23,5 +25,7 @@ button.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+/* This function slows down the video by 15% 1 being 100% */
 
 video.playbackRate = 0.85;
