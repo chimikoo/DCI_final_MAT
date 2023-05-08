45ef8a9f62353a5d167b65172828f4aea06a1310
Anntothekay <ankes.posteingang@gmail.com>
2023-05-08 08:32:55 +0200

    some refactoring

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,24 @@
 /* This function slows down the video by 15% 1 being 100% */
 const video = document.getElementById("background-video");
-video.playbackRate = 0.85;
-
 const container = document.getElementById("scroll-listener");
 const button = document.getElementById("back-to-top-button");
+let scrollTop;
 
 const scrollFunction = () => {
   if (scrollTop > 100) {
-    document.getElementById("back-to-top-button").style.display = "block";
+    button.style.display = "flex";
   } else {
-    document.getElementById("back-to-top-button").style.display = "none";
+    button.style.display = "none";
   }
 };
 
-let scrollTop;
 container.addEventListener("wheel", () => {
   scrollTop = container.scrollTop;
   scrollFunction();
 });
 button.addEventListener("click", () => {
   container.scrollTop = 0;
+  button.style.display = "none";
 });
+
+video.playbackRate = 0.85;
